20a2c0188b9d11ea9d10b882341df3d12437f178
Suzette Sousa <sousa.suzette.pro.com>
2020-12-05 15:59:17 +0100

    create functions

--- src/App.js
+++ src/App.js
@@ -12,6 +12,44 @@ class App extends Component {
     }
   }
 
+  onClick = button => {
+    if(button === "=") {
+      this.calculate()
+    } else if (button === "C") {
+      this.reset()
+    } else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  }
+
+  calculate = () => {
+    let checkResult = ''
+    if(this.state.result.includes('--')) {
+      checkResult = this.state.result.replace('--', '+');
+    } else {
+      checkResult = this.state.result
+    }
+    try {
+      this.setState({
+        //eslint-disable-next-line
+        result: (eval(checkResult))
+      })
+    }
+    catch (e) {
+      this.setState({
+        result: 'error'
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
   render() {
     return (
       <div>
