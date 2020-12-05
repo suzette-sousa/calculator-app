48d7f5ed35e31cc4edf43937b694bdbeb9ea9c4f
Suzette Sousa <sousa.suzette.pro.com>
2020-12-05 15:38:53 +0100

    create keypad component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Result from './components/Result';
+import Keypad from './components/Keypad';
 
 class App extends Component {
   constructor() {
@@ -16,6 +17,7 @@ class App extends Component {
       <div>
         <div className="calculator">
           <Result result={this.state.result} />
+          <Keypad onClick={this.onClick} />
         </div>
       </div>
     )
