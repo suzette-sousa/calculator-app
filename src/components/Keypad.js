97f3cc8622b57bbf8dc2e89bfb1a74511f8ad7d7
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-08 17:12:03 +0100

    group btn by type

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -8,79 +8,113 @@ class Keypad extends Component {
       {
         btn: '0',
         id: 'zero',
+        type: 'number'
       },
       {
         btn: '1',
         id: 'one',
+        type: 'number'
       },
       {
         btn: '2',
         id: 'two',
+        type: 'number'
       },
       {
         btn: '3',
         id: 'three',
+        type: 'number'
       },
       {
         btn: '4',
         id: 'four',
+        type: 'number'
       },
       {
         btn: '5',
         id: 'five',
+        type: 'number'
       },
       {
         btn: '6',
         id: 'six',
+        type: 'number'
       },
       {
         btn: '7',
         id: 'seven',
+        type: 'number'
       },
       {
         btn: '8',
         id: 'eight',
+        type: 'number'
       },
       {
         btn: '9',
         id: 'nine',
+        type: 'number'
       },
       {
         btn: '.',
         id: 'decimal',
+        type: 'number'
       },
       {
         btn: '+',
         id: 'add',
+        type: 'op'
       },
       {
         btn: '-',
         id: 'subtract',
+        type: 'op'
       },
       {
         btn: '*',
         id: 'multiply',
+        type: 'op'
       },
       {
         btn: '/',
         id: 'divide',
+        type: 'op'
       },
       {
         btn: '=',
         id: 'equals',
+        type: 'op'
       },
       {
         btn: 'ac',
         id: 'clear',
+        type: 'other'
       },
       {
         btn: '←',
         id: 'delete',
+        type: 'other'
       }
     ];
-    const calculatorBtns = buttons.map(({ btn, id }) => (
-      <button key={id} id={id} value={btn} onClick={this.props.handleButton}>{btn}</button>
+
+    function groupArrayOfObjects(list, key) {
+      return list.reduce(function(rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+      }, {});
+    };
+    
+    let calculatorBtnsCatg = groupArrayOfObjects(buttons,"type");
+
+    const calculatorBtns = Object.keys(calculatorBtnsCatg).map((item) => (
+      <div className={item}>
+        {calculatorBtnsCatg[item].map(({ btn, id }) => (
+          <button key={id} value={btn} onClick={this.props.handleButton}>{btn}</button>
+        ))}
+      </div>
     ));
+    ;
+
     return (
       <div className="buttons">{calculatorBtns}</div>
     )
